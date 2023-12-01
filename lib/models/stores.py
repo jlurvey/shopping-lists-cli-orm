b02f996b76e76bd23d41922e5f8e5a7afa06ef4d
Joseph Lurvey <joeylurvey@gmail.com>
2023-12-01 18:53:13 -0500

    Add department class constructor method

--- lib/models/stores.py
+++ lib/models/stores.py
@@ -1,2 +1,13 @@
 # lib/models/stores.py
 from models.__init__ import CURSOR, CONN
+
+
+class Store:
+
+    # Dictionary of objects saved to the database.
+    all = {}
+
+    def __init__(self, name, category, id=None):
+        self.id = id
+        self.name = name
+        self.category = category
