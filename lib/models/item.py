efd31e20d606d3a393d207e8dfd995b928ea303c
Joseph Lurvey <joeylurvey@gmail.com>
2023-12-02 09:09:15 -0500

    Implement name property with setter method

--- lib/models/item.py
+++ lib/models/item.py
@@ -15,3 +15,18 @@ class Store:
 
     def __repr__(self):
         return f'Item {self.id}: {self.name}, {self.need}, {self.store_id}'
+    
+    @property
+    def name(self):
+        return self.name
+    
+    @name.setter
+    def name(self, name):
+        if isinstance(name, str) and len(name):
+            self._name = name
+        else:
+            raise ValueError(
+                "Name must be non-empty string"
+            )
+        
+    
