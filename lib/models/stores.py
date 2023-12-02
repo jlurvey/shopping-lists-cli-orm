46728f24320513b5b82acbe2705b33444d48c67b
Joseph Lurvey <joeylurvey@gmail.com>
2023-12-01 22:27:16 -0500

    Define create_table class method

--- lib/models/stores.py
+++ lib/models/stores.py
@@ -12,37 +12,47 @@ class Store:
         self.id = id
         self.name = name
         self.category = category
-    
+
     # String representation method (dunder method)
     def __repr__(self):
         return f"<Store {self.id}: {self.name}, {self.category}>"
-    
-    # Name property 
+
+    # Name property
     @property
     def name(self):
         return self._name
-    
+
     @name.setter
     def name(self, name):
         if isinstance(name, str) and len(name):
-            self._name = name[0] + name[1:]
+            self._name = name[0].upper() + name[1:].lower()
         else:
             raise ValueError(
                 "Name must be a non-empty string"
             )
-        
+
     # Category property
     @property
     def category(self):
         return self._category
-    
+
     @category.setter
     def category(self, category):
         if isinstance(category, str) and len(category):
-            self._category = category[0] + category[1:]
+            self._category = category[0].upper() + category[1:].lower()
         else:
             raise ValueError(
                 "Category must be a non-empty string"
             )
-        
-    
+
+    @classmethod
+    def create_table(cls):
+        """Create a new table to persist the attributes of Store instances"""
+        sql = """
+            CREATE TABLE IF NOT EXISTS stores (
+            id INTEGER PRIMARY KEY,
+            name TEXT,
+            location TEXT)
+        """
+        CURSOR.execute(sql)
+        CONN.commit()
