78909cd04dbf6780b443f465f438600e662bc62b
Joseph Lurvey <joeylurvey@gmail.com>
2023-12-02 09:03:13 -0500

    Add Store class with initializer to manage database objects

--- lib/models/item.py
+++ lib/models/item.py
@@ -1,3 +1,14 @@
 # lib/models/item.py
 from models.__init__ import CURSOR, CONN
 from models.store import Store
+
+class Store:
+
+    #Dictionary of objects saved to the database
+    all = {}
+
+    def __init__(self, name, need, store_id, id=None):
+        self.id = id
+        self.name = name
+        self.need = need
+        self.store_id = store_id
