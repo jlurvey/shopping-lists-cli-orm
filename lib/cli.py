2d1f1ff0143543e35d3f04334f3864b94fc44a92
Joseph Lurvey <joeylurvey@gmail.com>
2023-12-04 20:48:09 -0500

    Add list of choice numbers and associated helper functions to be use in CLI

--- lib/cli.py
+++ lib/cli.py
@@ -21,12 +21,40 @@ from helpers import (
 
 def main():
     while True:
-        menu()
-        choice = input("> ")
-        if choice == "0":
-            exit_program()
-        else:
-            print("Invalid choice")
+            menu()
+            choice = input("> ")
+            if choice == "0":
+                exit_program()
+            elif choice == "1":
+                create_store()
+            elif choice == "2":
+                delete_store()
+            elif choice == "3":
+                list_stores()
+            elif choice == "4":
+                list_stores_by_category()
+            elif choice == "5":
+                find_store_by_name()
+            elif choice == "6":
+                find_store_by_id()
+            elif choice == "7":
+                update_store()
+            elif choice == "8":
+                create_item()
+            elif choice == "9":
+                delete_item()
+            elif choice == "10":
+                list_items()
+            elif choice == "11":
+                list_items_by_store()
+            elif choice == "12":
+                find_item_by_name()
+            elif choice == "13":
+                find_item_by_id()
+            elif choice == "14":
+                update_item()
+            else:
+                print("Invalid choice")
 
 
 def menu():
