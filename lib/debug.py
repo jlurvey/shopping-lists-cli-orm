562e239d924e8655a2a7a6758ad939479ecc2a6b
Joseph Lurvey <joeylurvey@gmail.com>
2023-12-05 21:04:17 -0500

    Define reset_database function

--- lib/debug.py
+++ lib/debug.py
@@ -2,7 +2,17 @@
 # lib/debug.py
 
 from models.__init__ import CONN, CURSOR
+from models.store import Store
+from .models.item import Item
 import ipdb
+
+def reset_database():
+    Item.drop_table()
+    Store.drop_table()
+    Item.create_table()
+    Store.create_table()
 
 
 ipdb.set_trace()
+
+reset_database()
